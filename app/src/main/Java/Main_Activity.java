4f41148c941e3a0930212508b8b50fa19db91ded
Nabozhev <nabozhev@mail.ru>
2015-06-25 22:53:40 +0300

    Create Man ACtivity and Layout + Manifest

---
+++ app/src/main/Java/Main_Activity.java
@@ -0,0 +1,16 @@
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.nabozhev.remindme.R;
+
+/**
+ * Created by Dmitry on 25.06.2015.
+ */
+public class Main_Activity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
